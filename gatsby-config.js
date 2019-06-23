8f34ec1209d25f567881d698ad2003c56e26bbd8
Omar De Los Santos <omar.dlsg@gmail.com>
2019-06-23 09:32:53 -0700

    Configure gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `WineFi`,
+    description: `Find a boozy place to get work done!`,
+    author: `@omarnyte`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `WineFi`,
+        short_name: `WineFi`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
