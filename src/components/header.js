ba0ff9dd81ac042138503cbc2a6381b60e166e6e
Omar De Los Santos <omarnyte@gmail.com>
2019-08-14 16:20:04 -0700

    Remove in-line styling

--- src/components/header.js
+++ src/components/header.js
@@ -3,27 +3,10 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#ca3e47`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 data-testid="site-title" style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header>
+    <div>
+      <h1 data-testid="site-title">
+        <Link to="/" >
           {siteTitle}
         </Link>
       </h1>
