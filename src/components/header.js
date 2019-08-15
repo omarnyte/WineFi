0edcfc3a9cf8f51b8f40ad44391e0fba482f3cff
Omar De Los Santos <omarnyte@gmail.com>
2019-08-14 17:03:29 -0700

    Style header

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,12 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import styles from "../styles/header.module.css"
+
 const Header = ({ siteTitle }) => (
-  <div>
-    <h1 data-testid="site-title">
-      <Link to="/" >
-        {siteTitle}
-      </Link>
+  <div className={styles.container}>
+    <h1 className={styles.title} data-testid="site-title">
+      <Link className={styles.siteTitleLink} to="/">{siteTitle}</Link>
     </h1>
   </div>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import Header from "./header"
+import Header from "./Header"
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
